924357f311f76ad9f8dcfdaab49a978624304f5d
Blake Mulnix <8352409+blakemulnix@users.noreply.github.com>
2023-11-28 03:33:54 +0000

    change up deployment

---
+++ nextjs-demo/startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Change to your project directory
+cd /home/site/wwwroot/nextjs-demo/
+
+# Install dependencies
+npm install
+
+# Build app
+npm run build
+
+# Start the application
+npm start

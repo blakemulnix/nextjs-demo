dbbf32b3171a4def7f970f055afe887a9becf24b
Blake Mulnix <8352409+blakemulnix@users.noreply.github.com>
2023-11-29 03:08:38 +0000

    fix up env vars

--- nextjs-demo/src/services/GithubGraphQLClient.tsx
+++ nextjs-demo/src/services/GithubGraphQLClient.tsx
@@ -1,14 +1,14 @@
 import { createHttpLink } from "@apollo/client";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+const GH_ACCESS_TOKEN = process.env.GH_ACCESS_TOKEN;
 
 const GithubGraphQLClient = new ApolloClient({
   ssrMode: true,
   link: createHttpLink({
     uri: "https://api.github.com/graphql",
     headers: {
-      Authorization: `Bearer ${GITHUB_TOKEN}`,
+      Authorization: `Bearer ${GH_ACCESS_TOKEN}`,
     },
   }),
   cache: new InMemoryCache(),

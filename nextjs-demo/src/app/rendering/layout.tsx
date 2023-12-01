33543f9e8009922e45ca22eadfe5a166a1287de4
Blake Mulnix <8352409+blakemulnix@users.noreply.github.com>
2023-12-01 20:23:03 +0000

    fix broken linke

--- nextjs-demo/src/app/rendering/layout.tsx
+++ nextjs-demo/src/app/rendering/layout.tsx
@@ -8,7 +8,7 @@ export default function RenderLayout({
   return (
     <div>
       <div className="flex flex-row space-x-2 justify-center">
-        <ButtonLink href="/rendering/cient">Client</ButtonLink>
+        <ButtonLink href="/rendering/client">Client</ButtonLink>
         <ButtonLink href="/rendering/static">Static</ButtonLink>
         <ButtonLink href="/rendering/server">Server</ButtonLink>
       </div>
